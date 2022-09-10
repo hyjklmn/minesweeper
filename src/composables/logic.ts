bfd744eace7b8b10798f9cb1d2decc822765738a
zhilei <858899665@qq.com>
2022-09-10 21:56:56 +0800

    refactor:update

---
+++ src/composables/logic.ts
@@ -0,0 +1,100 @@
+import { BlockState } from "~/types"
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+export class PlayGame {
+  state = ref<BlockState[][]>([])
+  constructor(public width: number, public height: number) {
+    this.reset()
+  }
+  reset() {
+    this.state.value = Array.from({ length: this.height }, (_, y) =>
+      Array.from(
+        { length: this.width },
+        (_, x): BlockState => ({ x, y, adjacentMines: 0, revealed: false })
+      )
+    )
+  }
+
+  generateMines(state: BlockState[][], initial: BlockState) {
+    for (const row of state) {
+      for (const block of row) {
+        if (Math.abs(initial.x - block.x) <= 1) continue
+        if (Math.abs(initial.y - block.y) <= 1) continue
+        block.mine = Math.random() < 0.3
+      }
+    }
+    this.updateNumbers()
+  }
+
+  updateNumbers() {
+    this.state.value.forEach((row) => {
+      row.forEach((block) => {
+        if (block.mine) return
+        this.getSiblings(block).forEach((b: BlockState) => {
+          if (b.mine) block.adjacentMines += 1
+        })
+      })
+    })
+  }
+
+  expendZero(block: BlockState) {
+    if (block.adjacentMines) return
+    this.getSiblings(block).forEach((s: BlockState) => {
+      if (!s.revealed) {
+        s.revealed = true
+        this.expendZero(s)
+      }
+    })
+  }
+
+  mineGenerated = false
+
+  onClick(block: BlockState) {
+    if (!this.mineGenerated) {
+      this.generateMines(this.state.value, block)
+      this.mineGenerated = true
+    }
+    block.revealed = true
+    if (block.mine) {
+      alert("boom")
+    }
+    this.expendZero(block)
+  }
+  onRightClick(block: BlockState) {
+    if (block.revealed) return
+    block.flagged = !block.flagged
+  }
+
+  getSiblings(block: BlockState) {
+    return directions
+      .map(([dx, dy]) => {
+        const x2 = block.x + dx
+        const y2 = block.y + dy
+        if (x2 < 0 || x2 >= this.width || y2 < 0 || y2 >= this.height)
+          return undefined
+        return this.state.value[y2][x2]
+      })
+      .filter(Boolean) as BlockState[]
+  }
+
+  checkGameState() {
+    if (!this.mineGenerated) return
+    const blocks = this.state.value.flat()
+    if (blocks.every((block) => block.flagged || block.revealed)) {
+      if (blocks.some((block) => block.flagged && !block.mine)) {
+        alert("You Cheat!")
+      } else {
+        alert("You Win!")
+      }
+    }
+  }
+}
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,104 +1,8 @@
 <script setup lang="ts">
-import { BlockState } from "~/types"
+import { PlayGame } from "~/composables/logic"
 import { toggleDev } from "~/composables"
-
-const WIDTH = 10
-const HEIGHT = 10
-const state = ref<BlockState[][]>([])
-
-function reset() {
-  state.value = Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from(
-      { length: WIDTH },
-      (_, x): BlockState => ({ x, y, adjacentMines: 0, revealed: false })
-    )
-  )
-}
-
-function generateMines(state: BlockState[][], initial: BlockState) {
-  for (const row of state) {
-    for (const block of row) {
-      if (Math.abs(initial.x - block.x) <= 1) continue
-      if (Math.abs(initial.y - block.y) <= 1) continue
-      block.mine = Math.random() < 0.3
-    }
-  }
-  updateNumbers(state)
-}
-const directions = [
-  [1, 1],
-  [1, 0],
-  [1, -1],
-  [0, -1],
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, 1],
-]
-
-function updateNumbers(state: BlockState[][]) {
-  state.forEach((row, y) => {
-    row.forEach((block, x) => {
-      if (block.mine) return
-      getSiblings(block).forEach((b: BlockState) => {
-        if (b.mine) block.adjacentMines += 1
-      })
-    })
-  })
-}
-
-function expendZero(block: BlockState) {
-  if (block.adjacentMines) return
-  getSiblings(block).forEach((s: BlockState) => {
-    if (!s.revealed) {
-      s.revealed = true
-      expendZero(s)
-    }
-  })
-}
-
-let mineGenerated = false
-
-function onClick(block: BlockState) {
-  if (!mineGenerated) {
-    generateMines(state.value, block)
-    mineGenerated = true
-  }
-  block.revealed = true
-  if (block.mine) {
-    alert("boom")
-  }
-  expendZero(block)
-}
-function onRightClick(block: BlockState) {
-  if (block.revealed) return
-  block.flagged = !block.flagged
-}
-
-function getSiblings(block: BlockState) {
-  return directions
-    .map(([dx, dy]) => {
-      const x2 = block.x + dx
-      const y2 = block.y + dy
-      if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT) return undefined
-      return state.value[y2][x2]
-    })
-    .filter(Boolean) as BlockState[]
-}
-
-function checkGameState() {
-  if (!mineGenerated) return
-  const blocks = state.value.flat()
-  if (blocks.every((block) => block.flagged || block.revealed)) {
-    if (blocks.some((block) => block.flagged && !block.mine)) {
-      alert("You Cheat!")
-    } else {
-      alert("You Win!")
-    }
-  }
-}
-reset()
-watchEffect(checkGameState)
+const play = new PlayGame(12, 12)
+const state = play.state
 </script>
 
 <template>
@@ -110,8 +14,8 @@ watchEffect(checkGameState)
           v-for="(block, x) in row"
           :key="x"
           :block="block"
-          @click="onClick(block)"
-          @contextmenu.prevent="onRightClick(block)"
+          @click="play.onClick(block)"
+          @contextmenu.prevent="play.onRightClick(block)"
         />
       </div>
     </div>
