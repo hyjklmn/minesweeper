067c34f5dc40b04317dcc2a5bf12cbfa52b54df4
zhilei <858899665@qq.com>
2022-09-16 21:08:41 +0800

    style:adjust paddding

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main font-sans p="x-4 y-4" text="center gray-700 dark:gray-200">
+  <main font-sans p="y-4" text="center gray-700 dark:gray-200">
     <router-view />
     <Footer />
   </main>
